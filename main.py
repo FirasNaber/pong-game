949830084369186958781077b581bc795d76858d
Firas Naber <firasnaber@gmail.com>
2022-09-24 17:46:45 -0500

    Improve ball and paddle detection

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while game_is_on:
         ball.bounce_y()
 
     # Bounce ball if it collides with a paddle
-    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+    if (ball.distance(r_paddle) < 55 and ball.xcor() > 320) or (ball.distance(l_paddle) < 55 and ball.xcor() < -320):
         ball.bounce_x()
 
     if ball.xcor() > 380:
